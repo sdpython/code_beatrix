d43be7d62b9c559db98d2a48cb7ec0da97b67ede
dupre <xavier.dupre@ensae.fr>
2015-03-23 01:30:00 +0100

    documentation

---
+++ src/code_beatrix/scratchs/__init__.py
@@ -0,0 +1,3 @@
+"""
+shortcuts for scratch
+"""
---
+++ src/code_beatrix/scratchs/example_echiquier/__init__.py
@@ -0,0 +1,15 @@
+"""
+Exercice à propos du parcours d'un échiquier.
+"""
+
+def check():
+    """
+    vérifie que certains fichiers sont ici
+    """
+    dirname = os.path.dirname(__file__)
+    f1 = os.path.exists(dirname, "echiquier.sb2")
+    if not os.path.exists(f1):
+        raise FileNotFoundError(f1)
+    f0 = os.path.exists(dirname, "echiquier0.sb2")
+    if not os.path.exists(f0):
+        raise FileNotFoundError(f0)        
