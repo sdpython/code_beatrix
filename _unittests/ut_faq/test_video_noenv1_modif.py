7345fa6520f8356b7569deb89494a7a322110e37
xavier dupré <xavier.dupre@gmail.com>
2018-01-02 23:05:34 +0100

    split unit test

--- _unittests/ut_faq/test_video_noenv1_modif.py
+++ _unittests/ut_faq/test_video_noenv1_modif.py
@@ -46,41 +46,21 @@ except ImportError:
 
 from pyquickhelper.loghelper import fLOG
 from pyquickhelper.pycode import get_temp_folder, ExtTestCase
-from src.code_beatrix.faq.faq_video import video_save, video_extract_video, video_compose, video_concatenate
+from src.code_beatrix.faq.faq_video import video_save, video_modification
 
 
-class TestVideo(ExtTestCase):
+class TestVideoModif(ExtTestCase):
 
-    def test_extract_avideo(self):
+    def test_modify_avideo(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
-        temp = get_temp_folder(__file__, "temp_video_extract")
+        temp = get_temp_folder(__file__, "temp_video_modification")
         vid = os.path.join(temp, '..', 'data', 'videxa.mp4')
-        vid2 = video_extract_video(vid, '00:00:01', '00:00:04')
-        exp = os.path.join(temp, "courte.mp4")
+        vid2 = video_modification(vid, speed=2., mirrory=True, mirrorx=True)
+        exp = os.path.join(temp, "courte2x.mp4")
         video_save(vid2, exp)
-        self.assertExists(exp)
-        exp = os.path.join(temp, "courte.gif")
-        video_save(vid2, exp)
-        self.assertExists(exp)
-
-    def test_compose_avideo(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-        temp = get_temp_folder(__file__, "temp_video_compose")
-        vid = os.path.join(temp, '..', 'data', 'videxa.mp4')
-        vid2 = video_compose(vid, vid, '00:00:01', '00:00:04')
-        exp = os.path.join(temp, "courte.mp4")
-        video_save(vid2, exp)
-        self.assertExists(exp)
-
-        exp = os.path.join(temp, "courte2.mp4")
-        vid3 = video_concatenate([vid, vid])
-        video_save(vid3, exp)
         self.assertExists(exp)
 
 
