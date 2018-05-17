912487e0dfe2d18e115509ed38257871a6b4bafb
xavier dupré <xavier.dupre@gmail.com>
2018-05-17 11:16:47 +0200

    fix documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import my_hachibee_sphinx_theme as hachibee_sphinx_theme
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
+import my_hachibee_sphinx_theme as hachibee_sphinx_theme
 
 
 set_sphinx_variables(__file__, "Les enfants codaient", "Xavier Dupré", 2018,
