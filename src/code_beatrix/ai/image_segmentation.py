7dcadc1cb793f3d8609a6a33e31f375ab31957c7
xavier dupré <xavier.dupre@gmail.com>
2021-01-03 12:51:06 +0100

    Update image_segmentation.py

--- src/code_beatrix/ai/image_segmentation.py
+++ src/code_beatrix/ai/image_segmentation.py
@@ -159,7 +159,7 @@ class DLImageSegmentation(DeepLearningImage):
         if preprocess:
             input, = fcn.datasets.transform_lsvrc2012_vgg16(
                 (feat,))  # pylint: disable=W0632
-            input = input[numpy.newaxis, :, :, :]  # pylint: disable=E0401
+            input = input[numpy.newaxis, :, :, :]  # pylint: disable=E0401,E1126
             return input
         else:
             return feat
