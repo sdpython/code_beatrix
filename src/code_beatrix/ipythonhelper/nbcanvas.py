920e93f67895c8f6c73762b0f3f0831f8ce3487f
xavier dupré <xavier.dupre@gmail.com>
2022-01-15 18:11:13 +0100

    Workaround  a change introduced in IPython 8

--- src/code_beatrix/ipythonhelper/nbcanvas.py
+++ src/code_beatrix/ipythonhelper/nbcanvas.py
@@ -2,10 +2,9 @@
 @file
 @brief Drawing from a canvas
 """
-
 import IPython
-from IPython.core.display import display_html, display_javascript, Javascript
-
+import IPython.core.display as ipydisplay
+from IPython.display import display_html, display_javascript, Javascript, HTML
 from .path_helper import local_d3js
 
 
@@ -60,7 +59,11 @@ def display_canvas_point(html_id, width=800, heigth=400, var_name="points"):
         .replace("__ID__", html_id)\
         .replace("__VARNAME__", var_name)
     js_libs = [js_libs]
-    display_html(IPython.core.display.HTML(data=html_src))
+    if 'display' not in dir(ipydisplay):
+        # Weird bug introduced in IPython 8.0.0
+        import IPython.core.display_functions
+        ipydisplay.display = IPython.core.display_functions.display
+    display_html(HTML(data=html_src))
     display_javascript(Javascript(data=test_d3_js, lib=js_libs))
 
     return points
