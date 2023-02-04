e39f8ae416c23940c1a227c11c667c19104b2ff4
xavier dupré <xavier.dupre@gmail.com>
2023-02-04 13:11:45 +0100

    Update test_code_style.py

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -16,7 +16,7 @@ class TestCodeStyle(ExtTestCase):
         check_pep8(src_, fLOG=fLOG,
                    pylint_ignore=('C0103', 'C1801', 'C2801', 'R1705', 'W0108', 'W0613',
                                   'C0111', 'W0212', 'W0622', 'W0703', 'W0201',
-                                  'W0221', 'W0223', 'W0107',
+                                  'W0221', 'W0223', 'W0107', 'R1735',
                                   'C0415', 'C0209'),
                    skip=["No name 'rectangle' in module 'cv2'",
                          "No name 'blur' in module 'cv2'",
@@ -34,7 +34,7 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="((temp_.*)|(.*test_videodl_noenv4_face.*))",
                    pylint_ignore=('C0103', 'C1801', 'C2801', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0212', 'W0622', 'W0107',
+                                  'C0111', 'W0212', 'W0622', 'W0107', 'R1735',
                                   'C0415', 'C0209'),
                    skip=[])
 
